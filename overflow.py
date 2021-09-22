c0baf0798f9fd4b9ea5dcc93dbce79c801dda886
kilic <kiliconu@itu.edu.tr>
2021-09-22 17:58:19 +0300

    calculate overflow size

---
+++ overflow.py
@@ -0,0 +1,30 @@
+def max_el(bit_len):
+    return (1 << bit_len) - 1
+
+
+k_a = 1
+k_b = 1
+bit_len = 8
+p_bit_len = [bit_len, bit_len, bit_len, bit_len - 1]
+p = [max_el(bit_len) for bit_len in p_bit_len]
+q = (1 << bit_len) - 1
+
+a = (1 << bit_len) - 1
+b = (1 << bit_len) - 1
+a = a * k_a
+b = b * k_a
+
+ab = a * b
+
+t0 = ab + q * p[0]
+t1 = 2 * ab + q * p[0] + q * p[1]
+t2 = 3 * ab + q * p[0] + q * p[1] + q * p[2]
+t3 = 4 * ab + q * p[0] + q * p[1] + q * p[2] + q * p[3]
+
+u0 = t0 + (t1 << bit_len)
+u1 = t2 + (t3 << bit_len)
+u0 = u0 >> (2 * bit_len)
+u1 = u1 >> (2 * bit_len)
+
+print(u0.bit_length())
+print(u1.bit_length())
