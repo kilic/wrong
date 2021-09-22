11075517f4b817ed069cc8f38655b160115e9aa7
kilic <kiliconu@itu.edu.tr>
2021-09-22 18:47:18 +0300

    overflow size for reduction

--- overflow.py
+++ overflow.py
@@ -2,9 +2,12 @@ def max_el(bit_len):
     return (1 << bit_len) - 1
 
 
+bit_len = 8
+
+print("mul")
+
 k_a = 1
 k_b = 1
-bit_len = 8
 p_bit_len = [bit_len, bit_len, bit_len, bit_len - 1]
 p = [max_el(bit_len) for bit_len in p_bit_len]
 q = (1 << bit_len) - 1
@@ -23,6 +26,26 @@ t3 = 4 * ab + q * p[0] + q * p[1] + q * p[2] + q * p[3]
 
 u0 = t0 + (t1 << bit_len)
 u1 = t2 + (t3 << bit_len)
+u0 = u0 >> (2 * bit_len)
+u1 = u1 >> (2 * bit_len)
+
+print(u0.bit_length())
+print(u1.bit_length())
+
+print("red")
+
+import sympy
+
+bit_len_modulus = bit_len * 4 - 1
+a = (1 << bit_len) - 1
+k_a = 10
+
+a = a * k_a
+q = k_a
+t = a + q * p[0]
+
+u0 = t + (t << bit_len)
+u1 = t + (t << bit_len)
 u0 = u0 >> (2 * bit_len)
 u1 = u1 >> (2 * bit_len)
 
