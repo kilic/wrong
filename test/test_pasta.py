275240e3f09aeb627ab1e8da76b8dc1a568dea34
kilic <kiliconu@itu.edu.tr>
2021-12-25 06:03:55 +0300

    add pasta tests

---
+++ test/test_pasta.py
@@ -0,0 +1,40 @@
+from wrong.limb import Limb
+from wrong.rns import RNS, Range
+from wrong.ecc import *
+from test import *
+
+number_of_limbs = 4
+limb_size = 68
+
+native_modulus = pasta_n
+wrong_modulus = pasta_p
+
+
+def test_various_bases():
+
+    wrong_modulus = pasta_n    # self emulating
+    RNS.setup(limb_size, wrong_modulus, native_modulus, number_of_limbs)
+    wrong_modulus = bn254_n
+    RNS.setup(limb_size, wrong_modulus, native_modulus, number_of_limbs)
+    wrong_modulus = bn254_p
+    RNS.setup(limb_size, wrong_modulus, native_modulus, number_of_limbs)
+    wrong_modulus = secp256k1_n
+    RNS.setup(limb_size, wrong_modulus, native_modulus, number_of_limbs)
+    wrong_modulus = secp256k1_p
+    RNS.setup(limb_size, wrong_modulus, native_modulus, number_of_limbs)
+
+
+def test_rns():
+    rns(wrong_modulus, native_modulus)
+
+
+def test_aux():
+    aux(wrong_modulus, native_modulus)
+
+
+def test_operations():
+    operations(wrong_modulus, native_modulus)
+
+
+def test_residues():
+    residues(wrong_modulus, native_modulus)
